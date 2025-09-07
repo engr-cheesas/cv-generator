9108650070f9500a9bd84059e65781c3422dfcee
nicolefranchesca <nicolefranchescausi@gmail.com>
2025-09-07 13:01:45 +0800

    Color and Design Update

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -7,7 +7,7 @@ const ProjectSection = ({projectInfo, updateProjectInfo, projIndex, addProjectDe
                 name="projectName"
                 value={projectInfo.projectName}
                 onChange={(e) => updateProjectInfo(e, projIndex)}
-                className="rounded-md px-4 py-2 w-full bg-stone-100 border border-lime-900 focus:ring-lime-600 text-stone-800 transition"/>
+                className="font-inter rounded-md px-4 py-2 w-full bg-stone-100 border border-lime-900 focus:ring-lime-600 text-stone-800 transition"/>
         
             <div className="flex gap-2">
                 <input type="text" 
@@ -15,7 +15,7 @@ const ProjectSection = ({projectInfo, updateProjectInfo, projIndex, addProjectDe
                 name="inputDef"
                 value={projectInfo.inputDef}
                 onChange={(e) => updateProjectInfo(e, projIndex)}
-                className="rounded-md px-4 py-2 w-full bg-stone-100 border border-lime-900 focus:ring-lime-600 text-stone-800 transition"/>
+                className="font-inter rounded-md px-4 py-2 w-full bg-stone-100 border border-lime-900 focus:ring-lime-600 text-stone-800 transition"/>
             
                 <button type="button"
                     onClick={() => addProjectDef(projIndex)}
--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -11,7 +11,7 @@ const SkillSection = ({skillInfo, onChange, addSkill, deleteSkill, toggleEditSki
                         onClick={() => !skillInfo.isEditing && toggleEditSkill(index)}
                         name="skillCategory"
                         onChange={(e) => onChange(index, "skillCategory", e.target.value)}
-                        className="w-1/4 rounded-md px-4 py-2 bg-stone-100 border border-lime-900 focus:ring-lime-600 text-stone-800 transition">
+                        className="w-1/4 font-inter rounded-md px-4 py-2 bg-stone-100 border border-lime-900 focus:ring-lime-600 text-stone-800 transition">
                     </input >
                     <input type="text"
                         placeholder="List of Skills"
@@ -20,7 +20,7 @@ const SkillSection = ({skillInfo, onChange, addSkill, deleteSkill, toggleEditSki
                         readOnly={!skillInfo.isEditing}
                         name="skillName"
                         onChange={(e) => onChange(index, "skillName", e.target.value)}
-                        className="w-3/4 bg-stone-100 border border-lime-900 focus:ring-lime-600 text-stone-800 rounded-md px-4 py-2 transition">
+                        className="w-3/4 font-inter bg-stone-100 border border-lime-900 focus:ring-lime-600 text-stone-800 rounded-md px-4 py-2 transition">
                     </input>
                     {!skillInfo.isEditing && (
                         <button type="button"
